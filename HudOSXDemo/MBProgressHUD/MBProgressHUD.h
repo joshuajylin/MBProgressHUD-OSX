9f1137099becb645242630ab6d6475f1b8bc07b9
Joshua Lin <joshua.lin@authentrend.com>
2019-11-14 19:09:09 +0800

    adds framework target and scheme

---
+++ HudOSXDemo/MBProgressHUD/MBProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  MBProgressHUD.h
+//  MBProgressHUD
+//
+//  Created by Joshua on 2019/11/14.
+//  Copyright © 2019 Wayne Fox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MBProgressHUD.
+FOUNDATION_EXPORT double MBProgressHUDVersionNumber;
+
+//! Project version string for MBProgressHUD.
+FOUNDATION_EXPORT const unsigned char MBProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBProgressHUD/PublicHeader.h>
+
+
